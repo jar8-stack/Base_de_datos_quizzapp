b3c29a58d1c7f655331d7e50bb2ac785326bb88a
jar8-stack <bioshock450226@hotmsil.com>
2020-11-02 14:36:39 -0600

    Consultas referentes a las respuestas

--- quizzapp.sql
+++ quizzapp.sql
@@ -47,3 +47,17 @@ INSERT INTO question_answers(id, text, correct_answer) VALUES(1, '1985', 1);
 INSERT INTO question_answers(id, text, correct_answer) VALUES(1, '1983', 0);
 INSERT INTO question_answers(id, text, correct_answer) VALUES(1, '1989', 0);
 INSERT INTO question_answers(id, text, correct_answer) VALUES(1, '1981', 0);
+
+-- Consultas generales del juego 
+
+--Mostrar temas del juego 
+SELECT * FROM themes; 
+
+--Mostrar respuestas de una pregunta el 1 es el id de la pregunta 
+SELECT text, correct_answer FROM question_answers WHERE id=1
+
+
+--Mostrar la respuesta correcta
+SELECT text, correct_answer FROM question_answers WHERE id=1 and correct_answer=1 
+
+
